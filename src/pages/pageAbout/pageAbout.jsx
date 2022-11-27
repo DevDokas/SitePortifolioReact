d2926b03c22ccc53af0fb7cf44beaaebf4eb53dd
Dokas <iguinhogms@gmail.com>
2022-11-27 03:04:06 -0300

    v1.1

--- src/pages/pageAbout/pageAbout.jsx
+++ src/pages/pageAbout/pageAbout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import Nav from '../components/Nav/Nav.jsx'
 
+import './styles/styleAbout.css'
+
 export default function About() {
     return(
         <React.StrictMode>
--- src/pages/pageHome/pageHome.jsx
+++ src/pages/pageHome/pageHome.jsx
@@ -1,11 +1,38 @@
+//React & Etc
 import React from "react";
 
+//Components
 import Nav from "../components/Nav/Nav.jsx";
+
+//Styles
+import './styles/styleHome.css'
+import Background from '../../assets/background.png'
 
 export default function Home() {
     return(
-        <React.StrictMode>
-            <Nav />
-        </React.StrictMode>
+    <React.StrictMode>
+        <HomeApp />
+    </React.StrictMode>
     )
 }
+
+function HomeApp() {
+    return(
+        <div style={{
+        backgroundImage: `url(${Background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100vw',
+        /* marginTop: '-35px' */
+        }}>
+        
+        <Nav />
+        <div className="screen">
+            <p className="DevDokasParagraph"><span className="TextColorOne">devdokas@devdokas-linux</span>:<span className="TextColorTwo">~</span>$</p>
+        </div>    
+        </div>
+    )
+}
+
