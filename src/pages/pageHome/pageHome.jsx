7136ce60c7d922d593c92576abf9a0ee3967d72c
Igor Gomes <iguinhogms@gmail.com>
2022-12-16 21:23:08 -0300

    Redux v0.0.5

--- src/pages/pageHome/pageHome.jsx
+++ src/pages/pageHome/pageHome.jsx
@@ -6,7 +6,7 @@ import Nav from "../components/Nav/Nav.jsx";
 
 //Styles
 import './styles/styleHome.css'
-import BackgroundVideo from '../../assets/videos/space-background.mp4'
+/* import BackgroundVideo from '../../assets/videos/space-background.mp4' */
 /* import Background from '../../assets/background.png' */
 
 export default function Home() {
@@ -22,11 +22,11 @@ function HomeApp() {
     return(
         <>
         
-        <video src={BackgroundVideo} autoPlay loop muted></video>
+        {/* <video src={BackgroundVideo} autoPlay loop muted></video> */}
         <Nav />
-        <div className="screen">
+{/*         <div className="screen">
             <p className="DevDokasParagraph"><span className="TextColorOne">devdokas@devdokas-linux</span>:<span className="TextColorTwo">~</span>$</p>
-        </div>    
+        </div>  */}    
         </>
     )
 }
